0469751062f1810e2365c14c7733f54e118098a9
connergillette <connerwgillette@gmail.com>
2023-05-24 12:06:13 -0700

    Add date to Entry component

--- app/components/Entry.tsx
+++ app/components/Entry.tsx
@@ -45,9 +45,14 @@ export default function Entry({ data }) {
   const {bg, border} = dynamicColors[data.day_rating - 1]
 
   return (
-    <div className={`flex h-16 rounded-lg border-solid border-2 px-5 gap-5 text-black ${bg} ${border}`}>
-      <span className="self-center">{data.body}</span>
-      <span className="self-center grow text-right">{data.day_rating}</span>
+    <div className="flex flex-col gap-2">
+      <div>
+        <span className="self-center opacity-30 text-xs tracking-widest">{new Date(data.created_at).toLocaleDateString()}</span>
+      </div>
+      <div className={`flex h-16 rounded-lg border-solid border-2 px-5 gap-5 text-black grow ${bg} ${border}`}>
+        <span className="self-center">{data.body}</span>
+        <span className="self-center grow text-right">{data.day_rating}</span>
+      </div>
     </div>
   )
 }
