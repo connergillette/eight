8eff34c1f881a129c17c0f7923d8127766ab2a3c
connergillette <connerwgillette@gmail.com>
2023-05-23 22:54:22 -0700

    Add basic functionality

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { ActionFunction, LoaderArgs, LoaderFunction, V2_MetaFunction, json, redirect } from "@remix-run/node"
+import { supabase } from '../server/supabase.server'
+import { Form, useActionData, useLoaderData } from '@remix-run/react';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -7,10 +9,48 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+export const action: ActionFunction = async ({ request }: LoaderArgs) => {
+  const data = await request.formData()
+  const body = data.get('body')
+  const dayRating = data.get('day_rating')
+  try {
+    const response = await supabase.from('entries').insert({ body, day_rating: dayRating })
+    if (response.status !== 201) {
+      return { error: 'Entry could not be saved.'}
+    }
+  } catch (e) {
+    return { error: 'Something went wrong.'}
+  }
+  
+  return redirect('/')
+}
+
+export const loader: LoaderFunction = async ({ request }: LoaderArgs) => {
+  const entries = (await supabase.from('entries').select()).data
+
+  console.log(entries)
+
+  return json({ entries })
+}
+
 export default function Index() {
+  const { entries } = useLoaderData<typeof loader>()
+  const error = useActionData<typeof action>()
+
   return (
     <div>
       <h1>Eight</h1>
+      {error ? <span className="text-red-500">{error.error}</span> : null}
+      <Form method="post">
+        <div className="flex flex-col">
+          <input name="body" placeholder="What did you do today?" />
+          <input name="day_rating" placeholder="How would you rate today?" type="number" min="1" max="10" />
+          <button type="submit">Submit</button>
+        </div>
+      </Form>
+      {
+        entries.map((item) => <div>{item.body}</div>)
+      }
     </div>
   );
 }
